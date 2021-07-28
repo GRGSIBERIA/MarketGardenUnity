46a1e3649b24adc1604f259113b1b399845236b1
Eiichi Takebuchi <nanashi4129@gmail.com>
2021-07-28 16:06:58 +0900

    add geometry generator

---
+++ MarketGarden/Assets/Scripts/Editor/GeometryGenerator.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class GeometryGenerator : EditorWindow
+{
+    [MenuItem("Window/Generator/Geometry")]
+    void Generate()
+    {
+
+    }
+}
