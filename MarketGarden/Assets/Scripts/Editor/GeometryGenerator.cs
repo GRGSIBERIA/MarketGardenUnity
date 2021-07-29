e45c69cc523dbedd21b8fc7d9f4dedc675b34209
Eiichi Takebuchi <nanashi4129@gmail.com>
2021-07-29 18:43:33 +0900

    triangleの設定

--- MarketGarden/Assets/Scripts/Editor/GeometryGenerator.cs
+++ MarketGarden/Assets/Scripts/Editor/GeometryGenerator.cs
@@ -50,16 +50,43 @@ public class GeometryGenerator : EditorWindow
         {
             for (int w = 0; w < basemap.width; ++w)
             {
-                int idx = h * basemap.height + w;
+                int idx = h * basemap.width + w;
                 float xsin = Mathf.Sin(dtheta * w);
                 float ycos = Mathf.Cos(dtheta * w);
                 float x = xsin * radius;
                 float z = ycos * radius;
                 vertices[idx] = new Vector3(x, h, z);
                 colors[idx] = basemap.GetPixel(w, h);
-                normals[idx] = new Vector3(x, 0, z);
+                normals[idx] = new Vector3(xsin, 0, ycos);
             }
         }
+
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices;
+        mesh.normals = normals;
+        mesh.colors = colors;
+
+        int polygon_count = (basemap.width - 1) * (basemap.height - 1);
+        int triangle_count = polygon_count * 6;
+        int[] triangles = new int[triangle_count];
+        for (int h = 0; h < basemap.height - 1; ++h)
+        {
+            for (int w = 0; w < basemap.width - 1; ++w)
+            {
+                int a = basemap.width * h + w;
+                int b = basemap.width * h + w + 1;
+                int c = basemap.width * (h + 1) + w;
+                int d = basemap.width * (h + 1) + w + 1;
+                int idx = h * basemap.width + w;
+                triangles[idx * 6 + 0] = a;
+                triangles[idx * 6 + 1] = b;
+                triangles[idx * 6 + 2] = c;
+                triangles[idx * 6 + 3] = b;
+                triangles[idx * 6 + 4] = d;
+                triangles[idx * 6 + 5] = c;
+            }
+        }
+        mesh.triangles = triangles;
     }
 
     private void OnGUI()
